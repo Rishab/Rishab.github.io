b640fdd5845b3df556b89b2c7e1d1015c6b37270
RishabChawla <rishychawla@gmail.com>
2020-07-17 12:18:03 -0400

    added floating navbar upon scroll

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+// Add background to navbar after hero image
+$(window).on("scroll", function () {
+  var hero = document.getElementById("hero");
+  if ($(window).scrollTop() > hero.clientHeight) {
+    $("nav").addClass("nav-bg");
+  } else {
+    $("nav").removeClass("nav-bg");
+  }
+});
+
+// Only show the navbar when scrolling up and hide upon scroll down
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-75px";
+  }
+  prevScrollpos = currentScrollPos;
+};
